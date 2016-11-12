1f6f47915c2c4b9af3b1d0f5c8d1e85f8a873f96
pradeep <pradeep.techhub@gmail.com>
2016-11-13 00:32:45 +0530

    Initial Commit

---
+++ src/main/java/com/patel/pradeep/SpringbootSaopWsApplication.java
@@ -0,0 +1,12 @@
+package com.patel.pradeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSaopWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSaopWsApplication.class, args);
+	}
+}
